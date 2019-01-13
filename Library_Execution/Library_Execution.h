86ef11bf50a523d48eae20186adb8cb7847f3b36
galaxy-wizard <kozlov_sergey_aleksandrovitch@mail.ru>
2019-01-13 06:50:45 +0300

    Add project files.

---
+++ Library_Execution/Library_Execution.h
@@ -0,0 +1,32 @@
+﻿
+// Library_Execution.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CLibraryExecutionApplication:
+// See Library_Execution.cpp for the implementation of this class
+//
+
+class CLibraryExecutionApplication : public CWinApp
+{
+public:
+	CLibraryExecutionApplication();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CLibraryExecutionApplication theApp;
